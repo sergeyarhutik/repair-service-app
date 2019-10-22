b591df1a7f165a978614fe38a63ffb1b5674def1
siarhei_arkhutsik <sergey.arhutik@gmail.com>
2019-10-22 09:43:40 +0300

    Initial commit

---
+++ src/main/java/com/epam/brest/repairserviceapp/RepairServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.epam.brest.repairserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/brest/repairserviceapp/RepairServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.brest.repairserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
